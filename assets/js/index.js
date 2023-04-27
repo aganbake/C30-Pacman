d5ce6c5158f4871f1388d39bf7bec7484c687eec
aganbake <abdul.villarroel@gmail.com>
2023-04-26 23:08:04 -0400

    latest commit

---
+++ assets/js/index.js
@@ -0,0 +1,23 @@
+function mouse() {
+  document.getElementById("text2").style.display = "block";
+}
+
+function mouseOut() {
+  document.getElementById("text2").style.display = "none";
+}
+
+function pacman() {
+  document.getElementById("img").style.width = "100%";
+}
+
+function pacmanOut() {
+  document.getElementById("img").style.width = "20%";
+}
+
+function letra() {
+  document.getElementById("caja3").style.fontSize = "x-large";
+}
+
+function letraOut() {
+  document.getElementById("caja3").style.fontSize = "medium";
+}
